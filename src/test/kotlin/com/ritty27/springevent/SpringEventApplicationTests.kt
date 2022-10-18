e113f738b9932035171fa05d991ae999305ba0e9
ritty27 <jspark9812@gmail.com>
2022-10-18 11:45:53 +0900

    chore: init project

---
+++ src/test/kotlin/com/ritty27/springevent/SpringEventApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ritty27.springevent
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringEventApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
